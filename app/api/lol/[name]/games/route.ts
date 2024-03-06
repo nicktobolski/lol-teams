92c3e5b8581c3a9a3f92fc5115b7271322faaf9a
Nick Tobolski <nicktobolski@protonmail.com>
2024-03-05 16:31:40 -0800

    oops 2

--- app/api/lol/[name]/games/route.ts
+++ app/api/lol/[name]/games/route.ts
@@ -10,7 +10,7 @@ export async function GET(
       `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${params.name}/ids`
     );
     reqUrl.searchParams.set("api_key", process.env.RIOT_API_KEY ?? "");
-    reqUrl.searchParams.set("count", String(50));
+    reqUrl.searchParams.set("count", String(100));
     const data = await ky.get(reqUrl).json();
     return Response.json(data);
   } catch (error) {
